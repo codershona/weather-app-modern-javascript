6c3aaf5c72fd4cf87ce8abe113f441023e58029a
codershona <falgunimst95@gmail.com>
2020-03-07 22:06:02 +0100

    location input is working

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
-// function
 const api = {
   key: "478cc3c9e7cd7db831d6f004db3d70f7",
-  baseurl: "https://api.openweathermap.org/data/2.5/"
+  base: "https://api.openweathermap.org/data/2.5/"
 }
+
 const searchbox = document.querySelector('.search-box');
 searchbox.addEventListener('keypress', setQuery);
 
 function setQuery(evt) {
   if (evt.keyCode == 13) {
-     getResults(searchbox.value);
-    console.log(searchbox.value);
+    getResults(searchbox.value);
   }
 }
 
-
 function getResults (query) {
-   fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-   .then(weather => {
-     return weather.json();
-   }).then(displayResults);
+  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather => {
+      return weather.json();
+    }).then(displayResults);
+}
 
+function displayResults (weather) {
+  console.log(weather);
 
 }
